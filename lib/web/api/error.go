7792860ec7500100b9f20689b4aedbc918526234
JamesClonk <jamesclonk@jamesclonk.ch>
2026-04-03 14:26:55 +0200

    add error logging

--- lib/web/api/error.go
+++ lib/web/api/error.go
@@ -1,11 +1,13 @@
 package api
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/JamesClonk/home-info-dashboard/lib/web"
 )
 
 func Error(rw http.ResponseWriter, err error) {
+	log.Printf("error: %v", err)
 	web.Render().JSON(rw, http.StatusInternalServerError, err.Error())
 }
